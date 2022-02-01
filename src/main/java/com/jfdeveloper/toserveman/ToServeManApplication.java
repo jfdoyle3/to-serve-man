dfbb4099cd53252224ec2bb159d950842ab4e1fd
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-01-31 22:18:54 -0500

    added project

---
+++ src/main/java/com/jfdeveloper/toserveman/ToServeManApplication.java
@@ -0,0 +1,13 @@
+package com.jfdeveloper.toserveman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToServeManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToServeManApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jfdeveloper/toserveman/ToServeManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfdeveloper.toserveman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToServeManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
