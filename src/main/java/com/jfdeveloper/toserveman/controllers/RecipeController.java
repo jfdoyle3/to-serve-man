470d1833c72d518bd31ff7cb37f828795650eb32
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-03-27 21:42:46 -0400

    Update RecipeController.java

--- src/main/java/com/jfdeveloper/toserveman/controllers/RecipeController.java
+++ src/main/java/com/jfdeveloper/toserveman/controllers/RecipeController.java
@@ -3,10 +3,12 @@ package com.jfdeveloper.toserveman.controllers;
 
 import com.jfdeveloper.toserveman.entities.Recipe;
 import com.jfdeveloper.toserveman.repositories.RecipeRepository;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,62 +22,59 @@ public class RecipeController {
     private RecipeRepository repository;
 
     @GetMapping("/recipes")
-    public ResponseEntity<List<Recipe>> getAllRecipes(@RequestParam String title){
-        try{
+    public ResponseEntity<List<Recipe>> getAllRecipes(@RequestParam String title) {
+        try {
             List<Recipe> recipes = new ArrayList<Recipe>();
-            if(title==null)
+            if (title == null)
                 recipes.addAll(repository.findAll());
             else
                 recipes.addAll(repository.findByTitleContaining(title));
 
-            if(recipes.isEmpty())
+            if (recipes.isEmpty())
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
-            return new ResponseEntity<>(recipes,HttpStatus.OK);
+            return new ResponseEntity<>(recipes, HttpStatus.OK);
 
-        }catch(Exception e){
-            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PostMapping("/create")
-    public ResponseEntity<Recipe> createVehicle(@RequestBody Recipe newRecipe) {
+    public ResponseEntity<Recipe> createRecipe(@RequestBody Recipe newRecipe) {
         return new ResponseEntity<>(repository.save(newRecipe), HttpStatus.CREATED);
     }
 
     @PutMapping("/recipes/{id}")
-    public ResponseEntity<Recipe> updateRecipe(@PathVariable("id") String id,@RequestBody Recipe recipe){
-        Optional<Recipe> recipeData=repository.findById(id);
-        if(recipeData.isPresent()) {
-            Recipe _recipe = recipeData.get();
-            _recipe.setTitle(recipe.getTitle());
-            _recipe.setIngredients(recipe.getIngredients());
-            _recipe.setDirections(recipe.getDirections());
-            return new ResponseEntity<>(repository.save(_recipe), HttpStatus.OK);
-        }
-        else
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @ResponseBody
+    public Recipe updateRecipe(@PathVariable String id, @RequestBody @NotNull Recipe updates) {
+        Recipe location = repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        if (updates.getTitle() != null) location.setTitle(updates.getTitle());
+        if (updates.getIngredients() != null) location.setIngredients(updates.getIngredients());
+        if (updates.getDirections() != null) location.setDirections(updates.getDirections());
+
+        return repository.save(location);
     }
 
+
     @DeleteMapping("/recipes/{id}")
-    public ResponseEntity<Recipe> deleteRecipe(@PathVariable("id")String id){
-        try{
+    public ResponseEntity<Recipe> deleteRecipe(@PathVariable("id") String id) {
+        try {
             repository.deleteById(id);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
-        }catch(Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @DeleteMapping("/newBook")
-    public ResponseEntity<HttpStatus> deleteAllRecipes(){
-        try{
+    public ResponseEntity<HttpStatus> deleteAllRecipes() {
+        try {
             repository.deleteAll();
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }catch (Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
 }
